6edf4551bd51af6f3214d4f356ded314ba90bcd6
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-10-21 23:34:53 +0530

    seller_login_database
    seller login working fine, mysql running on the server is connected.

--- team_Tomorrow/seller_app/models.py
+++ team_Tomorrow/seller_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Seller(models.Model):
+    email=models.EmailField()
+    name=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
+    mobile=models.CharField(max_length=100)
+    secret=models.CharField(max_length=100)
+    file=models.CharField(max_length=100)
+    gender=models.CharField(max_length=100)
+    
+    class Meta:
+        db_table="seller_details"
+
 # Create your models here.
--- team_Tomorrow/seller_app/views.py
+++ team_Tomorrow/seller_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from seller_app.models import Seller
 
 def seller_login(request):
     return render(request,'seller_home/seller_login.html')
+
+
+def seller_home(request):
+    if(request.method=="POST"):
+        print("Successful")
+        email=request.POST['username']
+        seller_email = email
+        password=request.POST['password']
+        userData=Seller.objects.all()
+        for it in userData:
+            if(it.email==email and it.secret==password):
+                # context = create_listings(email)
+                return render(request, 'seller_home/seller_home.html')
+        # print(userData)
+    return render(request, 'seller_home/seller_login.html')
 
 # Create your views here.
