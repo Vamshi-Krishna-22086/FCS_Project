196615ce9acb75a3310209e6ea0ea94db5042e20
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-10-21 23:06:35 +0530

    database_connectivity

--- team_Tomorrow/team_Tomorrow/settings.py
+++ team_Tomorrow/team_Tomorrow/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = "team_Tomorrow.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tomorrowdb',
+        'USER': 'fcs',
+        'PASSWORD': 'Group@2023',
+        'PORT':3306,
+        'HOST':'192.168.3.7',
+        
     }
 }
 
